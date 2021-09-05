6e904d74b1d0f30e2dbf04bf0242aa8695a4965f
Lorenarms <lorenzo.joseph.dominguez@gmail.com>
2021-09-05 11:34:09 -0700

    Added a linked list program for practice

--- Lesson_01_Generics/Driver.cs
+++ Lesson_01_Generics/Driver.cs
@@ -6,12 +6,53 @@ using System.Threading.Tasks;
 
 namespace Lesson_01_Generics
 {
+    public class Utilities
+    {
+        public T Max<T> (T a, T b) where T : IComparable
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+    }
+
+    public class TheseThings<T> where T : IComparable
+    {
+        public T Max (T a, T b) 
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+    }
+
+
     class Driver
     {
 
         static void Main(string[] args)
         {
+            var intThings = new TheseThings<int>();
+            
+            var stringThings = new TheseThings<string>();
+            
+
+
+            var utils = new Utilities();
+            var numbers = new List<int>();
+            numbers.Add(10);
+            numbers.Add(12);
+
+            var strings = new List<string>();
+            strings.Add("A");
+            strings.Add("B");
+
+            Console.WriteLine(utils.Max(numbers.ElementAt<int>(0), numbers.ElementAt<int>(1)));
+            Console.WriteLine(utils.Max(strings.ElementAt<string>(0), strings.ElementAt<string>(1)));
+
+            Console.WriteLine(intThings.Max(numbers.ElementAt<int>(0), numbers.ElementAt<int>(1)));
+            Console.WriteLine(stringThings.Max(strings.ElementAt<string>(0), strings.ElementAt<string>(1)));
+
+
             Console.WriteLine("This works");
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Lesson_01_LinkedLists/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_01_LinkedLists
+{
+    class LinkedList
+    {
+        private Node head;
+
+        public class Node
+        {
+            private int data;
+            private Node next;
+
+            //constructor to create new node
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+            public void SetNextNode(Node node)
+            {
+                this.next = node;
+            }
+            public int GetData()
+            {
+                return this.data;
+            }
+            public Node GetNode()
+            {
+                return this.next;
+            }
+        }
+
+        public void Push(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            new_node.SetNextNode(head);
+            head = new_node;
+        }
+
+        public void SetHead(Node node)
+        {
+            this.head = node;
+            //if (node == this.head)
+            //{
+                
+            //}
+            
+        }
+        public Node GetHead()
+        {
+            return head;
+        }
+
+        
+
+        public void PrintList()
+        {
+            Node n = head;
+            while (n != null)
+            {
+                Console.Write(n.GetData() + " ");
+                n = n.GetNode();
+            }
+        }
+
+        
+
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var llist = new LinkedList();
+
+            llist.SetHead(new LinkedList.Node(1));
+            
+            
+            var second = new LinkedList.Node(2);
+            var third = new LinkedList.Node(3);
+
+            llist.GetHead().SetNextNode(second);
+            second.SetNextNode(third);
+
+            llist.PrintList();
+
+            llist.Push(4);
+
+            Console.WriteLine();
+
+            llist.PrintList();
+
+            Console.ReadKey();
+            
+        }
+    }
+}
