52704c071eeb97f74096988bd8855e3903d0391b
Lorenarms <lorenzo.joseph.dominguez@gmail.com>
2021-09-16 15:32:48 -0700

    Created quicksort algorithm

---
+++ Lesson_05_ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_05_ExtensionMethods
+{
+    
+
+    
+    class ExtensionMethods
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Program completed successfully.");
+            Console.ReadKey();
+        }
+    }
+}
