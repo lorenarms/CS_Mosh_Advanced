3dc54dbd13c2b5cceb6151fe3e4bf9b41382c389
Lorenarms <lorenzo.joseph.dominguez@gmail.com>
2021-09-20 11:28:36 -0700

    Changed hashtable

--- Lesson_08_LinkedLists2/Driver.cs
+++ Lesson_08_LinkedLists2/Driver.cs
@@ -12,43 +12,42 @@ namespace Lesson_08_LinkedLists2
     }
     public class Employee
     {
-        public string _firstName { get; set; }
-        public string _lastName { get; set; }
         public int _id { get; set; }
 
         public Employee()
         {
-            _firstName = null;
-            _lastName = null;
-            _id = 100000;
+           _id = 100000;
         }
 
-        public Employee(string fname, string lname, int id)
+        public Employee(int id)
         {
-            _firstName = fname;
-            _lastName = lname;
             _id = id;
         }
 
 
     }
 
+    public class IndexList
+    {
+        public List<Employee> empList;
+
+        IndexList()
+        {
+            empList = new List<Employee>();
+        }
+    }
+
     public class HashTable
     {
-        public List<IndexList> _empTable = new List<IndexList>();
-        public class IndexList
-        {
-            public List<Employee> _empIndexTable;
-
-            IndexList()
-            {
-                _empIndexTable = new List<Employee>(Constants.DEFAULT_SIZE);
-            }
-        }
+        public List<IndexList> empTable = new List<IndexList>(Constants.DEFAULT_SIZE);
+        
 
         public HashTable()
         {
-            _empTable = new List<IndexList>(Constants.DEFAULT_SIZE);
+            empTable = new List<IndexList>();
+           
+            
+            
         }
 
         public int Hash(int key)
@@ -58,14 +57,9 @@ namespace Lesson_08_LinkedLists2
 
         public void Insert(Employee emp)
         {
-            //IndexList l;
             int index = Hash(emp._id);
-            //Console.WriteLine("index: " + index);
-
-            var l = _empTable.ElementAt(index);
-            l._empIndexTable.Add(emp);
             
-            //_empTable.ElementAt(index)._empIndexTable.Add(emp);
+            empTable[index].empList.Add(emp);
         }
 
         public void PrintAll()
@@ -73,10 +67,9 @@ namespace Lesson_08_LinkedLists2
             IndexList l;
             for (int i = 0; i < Constants.DEFAULT_SIZE; i++)
             {
-                l = _empTable.ElementAt(i);
-                foreach(Employee e in l._empIndexTable)
+                foreach(Employee e in empTable[i].empList)
                 {
-                    Console.WriteLine(e._id);
+                    Console.WriteLine("ID: " + e._id);
                 }
             }
         }
@@ -91,24 +84,31 @@ namespace Lesson_08_LinkedLists2
         
         static void Main(string[] args)
         {
-            Random randomNumber = null;
-            randomNumber = new Random();
-
-            HashTable EmployeeTable = new HashTable();
-
-            for (int j = 0; j < 90; j++)
+            Hash hash = new Hash();
+            while (true)
             {
-                int id = randomNumber.Next(100000, 999999);
-
-                var emp = new Employee("John", "Doe", id);
-                
-                EmployeeTable.Insert(emp);
+                string line = Console.ReadLine();
+                char command = line[0];
+                string argument = line.Substring(1);
+                int.TryParse(argument, out int employeeNum);
+                switch (command)
+                {
+                    case '+':
+                        hash.add(employeeNum);
+                        break;
+                    case '-':
+                        hash.remove(employeeNum);
+                        break;
+                    case '?':
+                        Console.WriteLine(hash.contains(employeeNum) ? "YES" : "NO");
+                        break;
+                    case '#':
+                        Console.WriteLine(hash.count);
+                        break;
+                    case 'Q':
+                        return;
+                }
             }
-            Console.WriteLine("Employees added to hash table...");
-
-            EmployeeTable.PrintAll();
-
-            Console.ReadKey();
 
         }
     }
---
+++ Lesson_08_LinkedLists2/HashTable.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using static System.Console;
+
+interface IntSet
+{
+	void add(int i, bool count = false);  // add a number to a set, if not already present
+	void remove(int i);  // remove a number, if present in the set
+	bool contains(int i);  // return true if i is in the set
+						   //int count;  // return the number of values in the set
+}
+
+class List
+{
+	class Node
+	{
+		public int val;
+		public Node next = null;
+		public Node(int val, Node next)
+		{
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	Node head = null;
+	Node stream = null;
+	int length = 0;
+
+	public void prepend(int i)
+	{
+		Node n = new Node(i, head);
+		head = n;
+		length++;
+	}
+
+	// if int i is found in list:
+	//   out variable 'node' points to the Node containing i
+	//   out variable 'prev' points to the Node which points to 'node'
+	//     unless 'node' is head, in which case 'prev' is null
+	//   returns true
+	public bool Find(int i, out Node node, out Node prev)
+	{
+		node = prev = null;
+		Node curr = new Node(0, head); // useful purely for iteration
+		while (curr.next != null && curr.next.val != i)
+			curr = curr.next;
+		if (curr.next == null) return false; // not in list
+		prev = curr.next == head ? null : curr;
+		node = curr.next;
+		return true;
+	}
+
+	public bool erase(int i)
+	{
+		bool valueWasFound = Find(i, out Node node, out Node prev);
+		if (!valueWasFound) WriteLine("value not found!");
+		if (!valueWasFound) return false;
+		if (prev == null)
+			head = node.next;
+		else
+			prev.next = node.next;
+		node.next = null;
+		length--;
+		return true;
+	}
+
+	public void reset() { stream = head; }
+
+	public int? next()
+	{
+		if (stream == null) return null;
+		int i = stream.val;
+		stream = stream.next;
+		return i;
+	}
+
+}
+
+class Hash : IntSet
+{
+	List[] buckets;
+	int capacity = 2;
+	int maxLoadFactor = 5;
+	public int count = 0;
+
+	public Hash() { buckets = new List[capacity]; }
+
+	int hash(int i) => i % capacity;
+
+	void rehash()
+	{
+		List[] oldBuckets = buckets;
+
+		capacity = capacity * 2;
+		buckets = new List[capacity];
+		for (int i = 0; i < oldBuckets.Length; i++)
+		{
+			oldBuckets[i].reset();
+			while (oldBuckets[i].next() is int a)
+			{
+				//if ( hash(a) == i ) continue;
+				this.add(a, false);
+			}
+		}
+	}
+
+	public void add(int i, bool count = true)
+	{
+		if (this.count >= maxLoadFactor * capacity)
+			rehash();
+
+		int index = hash(i);
+		// create new list if bucket equals null
+		buckets[index] = buckets[index] ?? new List();
+		buckets[index].prepend(i);
+		if (count)
+			this.count++;
+	}
+
+	public void remove(int i)
+	{
+		int index = hash(i);
+		if (buckets[index].erase(i)) count--;
+	}
+
+	public bool contains(int i)
+	{
+		int index = hash(i);
+		buckets[index] = buckets[index] ?? new List();
+		return buckets[index].Find(i, out Node node, out Node prev);
+	}
+}
