3979103d552602d931d864060c9ecd170c91f5e4
Lorenarms <lorenzo.joseph.dominguez@gmail.com>
2021-09-30 07:58:37 -0700

    Minor update

---
+++ Lesson_06_SortingMethods/ISorting.cs
@@ -0,0 +1,8 @@
+﻿namespace Lesson_06_SortingMethods
+{
+    public interface ISorting
+    {
+        int Sort(int[] numbers, int i, int k);
+    }
+
+}
--- Lesson_06_SortingMethods/Sorting.cs
+++ Lesson_06_SortingMethods/Sorting.cs
@@ -1,9 +1,5 @@
 ﻿namespace Lesson_06_SortingMethods
 {
-    public interface ISorting
-    {
-        int Sort(int[] numbers, int i, int k);
-    }
 
     public class QuickSorter : ISorting
     {
