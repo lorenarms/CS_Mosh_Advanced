2867e80d7f6b8b76799d25732804da22a96dd95f
Lorenarms <lorenzo.joseph.dominguez@gmail.com>
2021-10-03 21:38:15 -0700

    Created hashtable app

--- Lesson_10_HashTable_02/Driver.cs
+++ Lesson_10_HashTable_02/Driver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +8,41 @@ using System.Threading.Tasks;
 
 namespace Lesson_10_HashTable_02
 {
+    class Employee
+    {
+        private int _id;
+        private string _firstName { get; set; }
+        private string _lastName { get; set; }
+
+        public Employee()
+        {
+
+        }
+        public Employee(int id, string name)
+        {
+            this._id = id;
+            string[] s = name.Split(' ');
+            _firstName = s[0];
+            _lastName = s[1];
+            Console.WriteLine("The new employee has been created!\n" + _lastName + ", " + _firstName);
+        }
+        public int GetID()
+        {
+            return this._id;
+        }
+        public string GetName()
+        {
+            return this._firstName + " " + this._lastName;
+        }
+    }
+
     class Driver
     {
         public static void Main(string[] args)
         {
+            var table = new Hashtable();
+            table = null;
+            ICollection keys = null;
             bool run = true;
             int selection;
             try
@@ -24,15 +56,25 @@ namespace Lesson_10_HashTable_02
                         case 1:
                         {
                             Console.Clear();
-                                Console.WriteLine("You wish to create a table.");
-                                Console.ReadKey();
-                                break;
+                            if (table == null)
+                            {
+                                table = new Hashtable();
+                                keys = table.Keys;
+                                    Console.WriteLine("A new table has been created!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("A table already exists.");
+                            }
+                            Console.ReadKey();
+                            break;
                         }
                         case 2:
                         {
                             Console.Clear();
                                 Console.WriteLine("You are adding an element.");
-                                Console.ReadKey();
+                                AddElement(table);
+                                //Console.ReadKey();
                                 break;
 
                         }
@@ -47,6 +89,18 @@ namespace Lesson_10_HashTable_02
                         {
                             Console.Clear();
                                 Console.WriteLine("Printing!");
+                                if (table != null)
+                                {
+                                    foreach(DictionaryEntry e in table)
+                                    {
+                                        var emp = (Employee) e.Value;
+                                        int thisId = emp.GetID();
+                                        string thisName = emp.GetName();
+                                        Console.WriteLine(thisId + " - " + thisName);
+                                        
+                                    }
+                                }
+                                
                                 Console.ReadKey();
                                 break;
 
@@ -93,8 +147,50 @@ namespace Lesson_10_HashTable_02
             
 
 
-            Console.WriteLine("Program end. Press any key to exit");
+            //Console.WriteLine("Program end. Press any key to exit");
             Console.ReadKey();
+        }
+
+        public static void AddElement(Hashtable table)
+        {
+            int newId = 0;
+            string fullName = null;
+            bool add = true;
+            while (add)
+            {
+                Console.Clear();
+                Console.Write("\nEnter the employee's first and\nlast name, separated by a space: ");
+                fullName = Console.ReadLine();
+                while (!fullName.Contains(' '))
+                {
+                    Console.Write("\nYou entered: " + fullName + ". Please check that you included " +
+                                      "the\nfirst name, a space, and the last name: ");
+                    fullName = Console.ReadLine();
+                }
+                Random rand = new Random();
+                newId = rand.Next(100, 999);
+                while (table.ContainsKey(newId))
+                {
+                    newId = rand.Next(100, 999);
+                }
+
+                Console.WriteLine("The new employee is " + fullName);
+                Console.WriteLine("Their employee ID will be: " + newId);
+                Console.WriteLine("Is this correct? y/n ");
+                string selection = Console.ReadLine();
+                if (selection == "y")
+                {
+                    add = false;
+                }
+            }
+
+            Employee newEmployee = new Employee(newId, fullName);
+            table.Add(newId, newEmployee);
+            
+            
+
+
+
         }
     }
 }
