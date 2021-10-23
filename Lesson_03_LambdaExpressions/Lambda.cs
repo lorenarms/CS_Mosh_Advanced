60d94473826e1924fac27b3553a05d6697ae1e7f
Lorenarms <lorenzo.joseph.dominguez@gmail.com>
2021-10-22 20:44:22 -0700

    More updates

--- Lesson_03_LambdaExpressions/Lambda.cs
+++ Lesson_03_LambdaExpressions/Lambda.cs
@@ -1,18 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lesson_03_LambdaExpressions
 {
+    public class Objects
+    {
+        public Objects()
+        {
+
+        }
+
+        public int MyMethod(Func<int, int, int, int> MyFunc)
+        {
+            return MyFunc(3, 4, 5);
+        }
+
+        public void MyActionMethod(Action<int, int> MyAction)
+        {
+            int a = 4;
+            int b = 5;
+            MyAction(a, b);
+            Console.WriteLine($"A: {a} B: {b}" );
+            //return 0;
+        }
+
+
+
+    }
     class Lambda
     {
+        public static void MyActionToPass(int a, int b)
+        {
+            a++;
+            b++;
+        }
+        public static int MyFuncToPass(int a, int b, int c)
+        {
+            return a * b * c;
+            //throw new NotImplementedException();
+        }
 
         static void Main(string[] args)
         {
+            var newObject = new Objects();
+            int funcNum = newObject.MyMethod(MyFuncToPass);
+            Console.WriteLine($"Func returned: {funcNum}" );
+
+            newObject.MyActionMethod(MyActionToPass);
+
+
+            // lambda expression
+            // use delegates like 'Func' and 'Action'
+
+            Func<int, int> square = number => number * number;
+
+            Func<int, int, int> doubleMult = (i, i1) => i * i1;
+
+            Func<int, int, int, int> tripleMult = (i, j, k) => i * j * k;
+
+            int num = square(3);
+
+            Console.WriteLine(square(3));
+
+            Console.WriteLine(doubleMult(2, 3));
+
+            Console.WriteLine(tripleMult(1, 2, 3));
+
+            Action<double> div = i =>
+            {
+                i += 10;
+                i *= i;
+                i = 12 / i;
+                Console.WriteLine($"'i' is now equal to {i:F3}");
+
+            };
+
+            div(2);
+
+
+
+
+            Console.WriteLine("Press 'enter' to exit.");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        
+
+
+        public void NewMethod()
+        {
             int c = 5;
-            
+
             Func<int, int> square = n => n * n;
 
             Action<int> mult = b => c = 10;
@@ -35,6 +119,8 @@ namespace Lesson_03_LambdaExpressions
 
             Console.ReadKey();
         }
+
+
 
     }
 }
--- Lesson_12_GeneralPractice/Program.cs
+++ Lesson_12_GeneralPractice/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Lesson_12_GeneralPractice.Libraries;
@@ -13,12 +14,52 @@ namespace Lesson_12_GeneralPractice
     {
         public void Start()
         {
-            var tesla = new Vehicle(2, 45000, "Tesla", new Doors());
 
-            tesla.ModifyDoors();
+            string[] names = { "Lawrence", "Angie", "Trinity", "Sofia" };
+            int num = 0;
 
-            Console.WriteLine($"The number of doors is now {tesla.GetDoors()}.");
-            
+            // loop try-catch block
+            while (num != 999)
+            {
+                try
+                {
+                    //var tesla = new Vehicle(2, 45000, "Tesla", new Doors());
+
+                    //tesla.ModifyDoors();
+
+                    //Console.WriteLine($"The number of doors is now {tesla.GetDoors()}.");
+
+
+                    // nullable types
+                    Nullable<DateTime> date = null;
+                    DateTime? dateTwo = null;
+
+                    Console.WriteLine(date.GetValueOrDefault());
+                    Console.WriteLine(date.HasValue);
+                    // Console.WriteLine(date.Value);
+
+
+                    // parse string to integer
+                    string str = Console.ReadLine();
+                    int.TryParse(str, out num);
+
+                    Console.WriteLine(names[num]);
+
+
+                }
+
+                // exception when index is out of bounds in array
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine("That index does not exist");
+                    Console.WriteLine(ex.Message);
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine("The object is null and a value cannot be displayed");
+                    // throw new Exception("New Exception was thrown");
+                }
+            }
 
 
             WriteLine("Press 'Enter' to Exit.");
